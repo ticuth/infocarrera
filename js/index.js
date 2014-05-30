9c7c5aec6f242c3e978faddb5dffe5d77fef8614
Gustavo <gflores@uthermosillo.edu.mx>
2014-05-30 10:52:03 -0700

    Auto hide menu

--- js/index.js
+++ js/index.js
@@ -30,6 +30,13 @@ var app = {
             shrinkScrollbars: 'scale',
             fadeScrollbars: true
         });*/
+
+        $("#navbar li a").click(function(event) {
+            // check if window is small enough so dropdown is created
+            $("#nav-collapse").removeClass("in").addClass("collapse");
+        });        
+        
+        
         this.bindEvents();
     },
     openExternal: function (url) {
