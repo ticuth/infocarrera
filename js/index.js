7d59ef569cd4397538fffd66ff9a3715c135506e
Gustavo <gflores@uthermosillo.edu.mx>
2014-06-17 13:30:28 -0700

    New changes

--- js/index.js
+++ js/index.js
@@ -1,27 +1,40 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
+/*jslint devel:true */
+/*jslint browser:true */
+/*global $*/
 
 var app = {
     // Application Constructor
+    escapeHTML: function (s) {
+        'use strict';
+        var MAP, repl;
+        MAP = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&#34;',
+            "'": '&#39;'
+        };
+        repl = function (c) { return MAP[c]; };
+        return s.replace(/[&<>'"]/g, repl);
+    },
+    
+    
     myScroll: {},
-    initialize: function() {
+    zoomOut: function (obj) {
+        'use strict';
+        $(obj).animate({height: '-=50', width: '-=50'});
+    },
+    zoomIn: function (obj) {
+        'use strict';
+        $(obj).animate({height: '+=50', width: '+=50'});
+        
+    },
+    exit: function () {
+        'use strict';
+        window.close();
+    },
+    initialize: function () {
+        'use strict';
         /*document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
         app.myScroll =  new IScroll('#divInfo', {
             scrollbars: true,
@@ -31,42 +44,49 @@ var app = {
             fadeScrollbars: true
         });*/
 
-        $("#navbar li a").click(function(event) {
+        $("#navbar li a").click(function (event) {
             // check if window is small enough so dropdown is created
             $("#nav-collapse").removeClass("in").addClass("collapse");
-        });        
+        });
         
         
         this.bindEvents();
+        app.getPage('content/principal.html');
     },
     openExternal: function (url) {
-        navigator.app.loadUrl(url, {openExternal : true});  
+        'use strict';
+        window.open(url, '_system');
     },
-    getExternalData: function(){
+    getExternalData: function () {
+        'use strict';
         $('#scroller').html('Conectando con servidor...');
 		$.ajax({
 			dataType: 'json',
 			url: 'http://www.tic-uth.net/gflores/ticinfo/',
-			data: {},
-		}).done(function(data){
+			data: {}
+		}).done(function (data) {
             console.log(data);
-			var tot = data.news.length;
-			var shtml = [], sf="";
-            var archivo = "";
-            var esArchivo = false;
-			for(var i = 0;i < tot; i ++) {
-				shtml.push('<div>'+data.news[i].fecha + ' ' + data.news[i].text+'<br>');
+            var tot, shtml, archivo, esArchivo, sf, i, f;
+			tot = data.news.length;
+			shtml = [];
+            sf = "";
+            archivo = "";
+            esArchivo = false;
+			for (i = 0; i < tot; i += 1) {
+				shtml.push('<div>' + data.news[i].fecha + ' ' + app.escapeHTML(data.news[i].text) + '<br>');
                 shtml.push('<ul>');
                 
-				for(var f = 0 ;f < data.news[i].files.length; f++){
+				for (f = 0; f < data.news[i].files.length; f += 1) {
                     sf = data.news[i].files[f];
                     
-                    archivo = sf.substring(sf.lastIndexOf('/')+1,sf.length);
-                    esArchivo = (sf.indexOf('.doc')>0)||(sf.indexOf('.pdf')>0)
-                    if (esArchivo){
-                        shtml.push('<li>Archivo: <a href="#" onclick="app.openExternal(\''+sf+'\')">'+archivo+'</a></li>');
-                    }else{
-                        shtml.push('<li>Liga: <a href="#" onclick="app.openExternal(\''+sf+'\')">">'+sf+'</a></li>');
+                    archivo = sf.substring(sf.lastIndexOf('/') + 1, sf.length);
+                    esArchivo = (sf.indexOf('.doc') > 0) || (sf.indexOf('.pdf') > 0);
+                    //console.log(sf);
+                    //console.log(archivo);
+                    if (esArchivo) {
+                        shtml.push('<li>Archivo: <a href="#" onclick="app.openExternal(\'' + sf + '\')">' + archivo + '</a></li>');
+                    } else {
+                        shtml.push('<li>Liga: <a href="#" onclick="app.openExternal(\'' + sf + '\')">' + sf + '</a></li>');
                     }
 					
 				}
@@ -77,24 +97,26 @@ var app = {
             console.log(shtml);
 			$('#scroller').html(shtml.join('\n'));
             
-		}).error(function(){
+		}).error(function () {
             $('#scroller').html('Error al conectar');
 		});
     },
-    getPageInto: function(url,whereto){
+    getPageInto: function (url, whereto) {
+        'use strict';
 		$.ajax({
 			dataType: 'text',
-			url: url,
-        }).success(function(data){
+			url: url
+        }).success(function (data) {
             $(whereto).html(data);
         });
         
     },
-    getPage: function(url){
+    getPage: function (url) {
+        'use strict';
 		$.ajax({
 			dataType: 'text',
-			url: url,
-        }).success(function(data){
+			url: url
+        }).success(function (data) {
             $('#scroller').html(data);
         });
         
@@ -103,23 +125,27 @@ var app = {
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
+        'use strict';
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
+        'use strict';
         /*app.receivedEvent('deviceready');*/
         /*app.getPageInto('content/menu.html','#appmenu');*/
-        app.getPageInto('content/principal.html','#scroller');
+        app.getPageInto('content/principal.html', '#scroller');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function (id) {
+        'use strict';
+        var parentElement, listeningElement, receivedElement;
+        parentElement = document.getElementById(id);
+        listeningElement = parentElement.querySelector('.listening');
+        receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
